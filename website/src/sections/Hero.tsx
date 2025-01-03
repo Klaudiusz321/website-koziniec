c8e1ed0cd21ca6ef8652ba2dae4f877452ba6b36
MarceliBil <marbil003@gmail.com>
2025-01-03 23:14:21 +0100

    navabr visibility fix / toggle bg color add / hero dark overlay improve

--- website/src/sections/Hero.tsx
+++ website/src/sections/Hero.tsx
@@ -30,7 +30,7 @@ const HeroSection: React.FC = () => {
       </div>
 
       {/* Dark Overlay */}
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="absolute inset-0 bg-black bg-opacity-70"></div>
     </section>
   );
 };
