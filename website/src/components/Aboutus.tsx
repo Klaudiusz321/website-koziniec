c66c6aacbea00fca0d2daff47a6a48dc96dff925
Klaudiusz321 <claudiuswebdesign@gmail.com>
2025-02-11 14:38:56 +0100

    album9

--- website/src/components/Aboutus.tsx
+++ website/src/components/Aboutus.tsx
@@ -6,7 +6,7 @@ const albumsData = [
   {
     id: 1,
     title: "Album",
-    mainImage: "/photos/zywiec_main.jpg", // Upewnij się, że zdjęcia są w public/photos
+    mainImage: "/dist/photos/zywiec_main.jpg", // Upewnij się, że zdjęcia są w public/photos
     location: "Żywiec",
     date: "14.07.2024",
   },
@@ -15,7 +15,8 @@ const albumsData = [
 const Aboutus: React.FC = () => {
   return (
     <section className="bg-black text-white py-16 px-8 md:px-16 lg:px-32">
-      <h2 className="text-4xl font-bold mb-12 text-center">Albumy z wydarzeń</h2>
+      <h2 className="text-4xl font-bold mb-12 text-center">Szukasz zdjęć ze zlotu lub gadżetów dla Mercedesiarza?
+      Dobrze trafiłeś! Rozglądnij się</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {albumsData.map((album) => (
           <Link to={`/album/${album.id}`} key={album.id}>
